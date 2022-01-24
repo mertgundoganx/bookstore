b1589cfcab66bec35f2575da5b880f05ea6de1fa
Mert Gundogan <mert.gundogan@outlook.com>
2022-01-24 14:53:52 +0300

    bookModel.js updated.

--- src/model/bookModel.js
+++ src/model/bookModel.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-    "name": {
+    "title": {
         type: String,
         unique: true,
         minlength: 2,
@@ -15,9 +15,54 @@ const bookSchema = new mongoose.Schema({
         maxlength: 50,
         required: true
     },
+    "pages": {
+        type: Number,
+        required: true,
+    },
     "year": {
         type: Number,
         required: true
+    },
+    "country": {
+        type: String,
+        minlength: 3,
+        maxlength: 50,
+        required: true
+    },
+    "imageLink": {
+        type: String,
+        minlength: 3,
+        maxlength: 100,
+        required: true
+    },
+    "language": {
+        type: String,
+        minlength: 3,
+        maxlength: 50,
+        required: true
+    },
+    "link": {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    "category": {
+        type: String,
+        default: "No Category"
+    },
+    "publisher": {
+        type: String,
+        minlength: 2,
+        maxlength: 50,
+        default: "No Publisher"
+    },
+    "stock": {
+        type: Number,
+        default: 100
+    },
+    "isOnSale": {
+        type: Boolean,
+        default: true
     }
 }, {collection: 'books', timestamps: true});
 
