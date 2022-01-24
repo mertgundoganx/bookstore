f61d6bcf5194bc099fe359349ab9d030e4f1fa15
Mert Gundogan <mert.gundogan@outlook.com>
2022-01-24 13:20:54 +0300

    endpoints updated.

--- src/router/booksRouter.js
+++ src/router/booksRouter.js
@@ -2,12 +2,9 @@
 const router = require('express').Router();
 const {getAllBooks, addBook, updateBook, deleteBook} = require("../controller/booksController");
 
-router.route('/')
-    .get(getAllBooks)
-    .post(addBook);
-
-router.route('/:id')
-    .patch(updateBook)
-    .delete(deleteBook);
+router.route('/').get(getAllBooks)
+router.route('/').post(addBook);
+router.route('/:id').patch(updateBook)
+router.route('/:id').delete(deleteBook);
 
 module.exports = router;
